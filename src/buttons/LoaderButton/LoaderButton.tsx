0fb8dc6fdf6c38fe783cad67503f748eeab8bb82
Usman <usaeed@bridgelinxpk.com>
2023-09-09 13:57:22 +0500

    set up animations: loader

--- src/buttons/LoaderButton/LoaderButton.tsx
+++ src/buttons/LoaderButton/LoaderButton.tsx
@@ -24,49 +24,50 @@ const LoaderButton = (props: LoaderButtonProps) => {
 
     // Created styled button widget
     const LoaderButtonWidget = styled.button`
-        display: flex;
-        justify-content: center;
-        font-family: 'Nunito Sans';
-        align-items: center;
-        text-align: center;
-        box-shadow: 0px 6px 13px rgba(0, 0, 0, 0.12);
-        outline: none !important;
-        border-radius: 50px;
-        border: none;
-        cursor: pointer;
-    `;
+    display: flex;
+    justify-content: center;
+    font-family: 'Nunito Sans';
+    align-items: center;
+    text-align: center;
+    box-shadow: 0px 6px 13px rgba(0, 0, 0, 0.12);
+    outline: none !important;
+    border-radius: 50px;
+    border: none;
+    cursor: pointer;
+    text-transform: ${textTransform || 'none'}; // Apply text-transform
+  `;
 
     const spinner = keyframes`
-        0% {
-            animation: spin 0.25s linear infinite;
-        }
-        100% {
-            animation: spin 0.5s linear infinite;
-        }
-    `
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    `;
 
     const SpinnerWidget = styled.div`
-        border: 3px solid #f3f3f3;
-        border-top: 3px solid #000000;
-        border-radius: 50%;
-        width: 24px;
-        height: 24px;
-        animation: ${spinner};
-    `;
+    border: 3px solid #f3f3f3;
+    border-top: 3px solid #000000;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    animation: ${spinner} 0.5s linear infinite;
+  `;
 
 
 
     // Button properties
     var buttonProperties = {
         backgroundColor: onHover ? hoverBackgroundColor : backgroundColor,
-        TextTransform: textTransform,
+        TextTransform: textTransform || 'none',
         width, height, borderRadius,
     }
 
     // Label properties
     var textProperties = {
         color: onHover ? hoverTextColor : textColor,
-        fontSize, fontWeight, TextTransform: textTransform,
+        fontSize, fontWeight, TextTransform: textTransform || 'none',
         paddingLeft: '10px',
         paddingRight: '10px'
     }
