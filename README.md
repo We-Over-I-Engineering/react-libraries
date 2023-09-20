f57e6ff8bacf57525b0e3efc9d472176ff840f1b
Usman <usaeed@bridgelinxpk.com>
2023-09-20 13:01:07 +0500

    changes in npm publishing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-libraries
 React libraries developed by highly talented team at We Over I.
+
+# to publish a package to npm: 
+1. npm login
+2. npm publish --access public
---
+++ buttons.js
@@ -0,0 +1,23 @@
+import CapsuleButton from './dist/components/CapsuleButton/index';
+import CircleButton from './dist/components/CircleButton/index';
+import GradientButton from './dist/components/GradientButton/index';
+import GradientIconButton from './dist/components/GradientIconButton/index';
+import IconButton from './dist/components/IconButton/index';
+import LoadingButton from './dist/components/LoadingButton/index';
+import MasterButton from './dist/components/MasterButton/index';
+import ParallelogramButton from './dist/components/ParallelogramButton/index';
+import RectangleButton from './dist/components/RectangleButton/index';
+import ToggleButton from './dist/components/ToggleButton/index';
+
+export {
+    CapsuleButton,
+    CircleButton,
+    GradientButton,
+    GradientIconButton,
+    IconButton,
+    LoadingButton,
+    MasterButton,
+    ParallelogramButton,
+    RectangleButton,
+    ToggleButton
+};
